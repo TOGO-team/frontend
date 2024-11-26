88f386f071f450a510bbe6c57151f8ea9517a88b
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-11-27 01:59:44 +0900

    feat: layout 배경 색상 추가

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full bg-gray-50">
-      <div className="relative mx-auto w-full max-w-lg h-full min-h-screen">
+      <div className="relative mx-auto w-full max-w-lg h-full min-h-screen bg-white">
         <main>{children}</main>
         <div id="portal" />
       </div>
