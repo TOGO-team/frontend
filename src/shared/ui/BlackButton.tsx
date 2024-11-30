90c4208f485eda7dd29e3cdebd1d97dbc7e6c6be
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-11-30 18:28:26 +0900

    fix: w-full, h-full 제거

--- src/shared/ui/BlackButton.tsx
+++ src/shared/ui/BlackButton.tsx
@@ -7,6 +7,6 @@ interface BlackButtonProps {
 }
 
 const BlackButton: React.FC<BlackButtonProps> = ({ width, height, text }) => {
-  return <button className={`w-full h-full ${width} ${height} bg-black rounded-[5px] text-white`}>{text}</button>;
+  return <button className={`${width} ${height} bg-black rounded-[5px] text-white`}>{text}</button>;
 };
 export default BlackButton;
