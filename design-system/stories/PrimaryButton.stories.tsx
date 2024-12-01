0a3d7ea81dc91e307e1469aedbe2178ac7a73f44
rachel4w2 <rachel4w2@naver.com>
2024-12-01 22:06:07 +0900

    feat: PrimaryButton 스토리 수정

--- design-system/stories/PrimaryButton.stories.tsx
+++ design-system/stories/PrimaryButton.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import PrimaryButton from '../ui/PrimaryButton';
-import { MemoryRouter } from 'react-router-dom';
 
 type Story = StoryObj<typeof meta>;
 
@@ -13,9 +12,7 @@ const meta = {
   decorators: [
     Story => (
       <div className="w-96">
-        <MemoryRouter>
-          <Story />
-        </MemoryRouter>
+        <Story />
       </div>
     ),
   ],
@@ -25,10 +22,6 @@ const meta = {
       control: 'text',
       description: '버튼 텍스트',
       defaultValue: '다음',
-    },
-    navigateTo: {
-      control: 'text',
-      description: '클릭 시 이동할 경로',
     },
     disabled: {
       control: 'boolean',
@@ -62,14 +55,5 @@ export const Disabled: Story = {
     label: '비활성화',
     onClick: () => console.log('This should not be logged'),
     disabled: true,
-  },
-};
-
-export const WithNavigation: Story = {
-  args: {
-    label: '이동',
-    navigateTo: '/example-path', // 경로 설정
-    onClick: () => console.log('Navigating...'),
-    disabled: false,
   },
 };
