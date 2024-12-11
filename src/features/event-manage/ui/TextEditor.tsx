4f8c4913ead8c3849c3b99310349c547fff34eb2
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-11 20:47:43 +0900

    feat: 이벤트 등록 페이지 퍼블리싱

---
+++ src/features/event-manage/ui/TextEditor.tsx
@@ -0,0 +1,61 @@
+// 사진 첨부는 추후에...
+import { useMemo, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const formats = [
+  'font',
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'align',
+  'color',
+  'background',
+  'size',
+  'h1',
+];
+
+const TextEditor = () => {
+  const [content, setContent] = useState('');
+
+  const handleChange = (value: string) => {
+    const newText = value.replace(/<\/?[^>]+(>|$)/g, ''); // 태그 제거
+    setContent(newText);
+    console.log(newText);
+  };
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+        ['link', 'image'],
+        [{ align: [] }, { color: [] }, { background: [] }],
+        ['clean'],
+      ],
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-start gap-2 mb-4">
+      <h1 className="font-bold text-black text-lg">이벤트에 대한 상세 설명</h1>
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        onChange={handleChange}
+        className="h-[300px] overflow-auto"
+      />
+    </div>
+  );
+};
+export default TextEditor;
--- src/shared/ui/backgrounds/EventRegisterLayout.tsx
+++ src/shared/ui/backgrounds/EventRegisterLayout.tsx
@@ -1,11 +1,32 @@
-const EventRegisterLayout = () => {
+import { ReactNode } from 'react';
+import Button from '../../../../design-system/ui/Button';
+import Header from '../../../../design-system/ui/Header';
+
+interface EventRegisterLayoutProps {
+  children: ReactNode;
+  title: string;
+  className?: string;
+}
+
+const EventRegisterLayout = ({ children, title, className }: EventRegisterLayoutProps) => {
+  const onBack = () => {};
+  const handleButtonClick = () => {};
   return (
-    <div className="h-screen bg-white relative">
+    <div className="flex flex-col justify-center items-center">
       {/* 헤더 영역 */}
+      <div className="w-full h-36 md:h-40 bg-gradient-to-br from-[#FF5593] to-[rgb(255,117,119)] rounded-b-[60px]">
+        <Header title="이벤트 등록" onBack={onBack} className="text-white" />
+      </div>
 
       {/* 레이아웃 내용 */}
-      <div className="relative h-40 md:h-44 bg-gradient-to-br from-[#FF5593] to-[rgb(255,117,119)] rounded-b-[60px]">
-        <div className="absolute left-1/2 transform -translate-x-1/2 top-[calc(100%-8vh)] w-[85%] min-h-[85vh] bg-white rounded-[20px]" />
+      <div className="relative flex flex-col justify-between -translate-y-16 w-[85%] min-h-[81vh] bg-white rounded-[20px]">
+        <div>
+          <div className="text-center w-full my-8 text-2xl font-bold">{title}</div>
+          <div className={`relative ${className}`}>{children}</div>
+        </div>
+        <div className="sticky bottom-0 left-0 w-full p-5">
+          <Button label="다음" onClick={handleButtonClick} className="w-full h-12 rounded-full" />
+        </div>
       </div>
     </div>
   );
