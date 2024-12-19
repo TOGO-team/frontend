0935d4b9baf49bb0e9e179f8378ff1d1abe439ec
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-20 00:31:15 +0900

    fix: 레이아웃 버그 수정

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,8 +1,8 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full min-h-screen h-full bg-gray-50 overflow-auto">
-      <div className="relative mx-auto w-full max-w-lg min-h-screen bg-white">
-        <main className="flex-grow">{children}</main>
+    <div className="w-full min-h-screen h-screen bg-gray-50 overflow-auto">
+      <div className="mx-auto w-full max-w-lg min-h-screen bg-white">
+        <main>{children}</main>
         <div id="portal" />
       </div>
     </div>
--- src/shared/ui/backgrounds/EventRegisterLayout.tsx
+++ src/shared/ui/backgrounds/EventRegisterLayout.tsx
@@ -10,21 +10,21 @@ interface EventRegisterLayoutProps {
   onPrev?: () => void;
 }
 
-const EventRegisterLayout = ({ children, title, className, onNext, onPrev }: EventRegisterLayoutProps) => {
+const EventRegisterLayout = ({ children, title, className = '', onNext, onPrev }: EventRegisterLayoutProps) => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="relative flex">
       {/* 헤더 영역 */}
-      <div className="w-full h-36 md:h-40 bg-gradient-to-br from-[#FF5593] to-[rgb(255,117,119)] rounded-b-[60px]">
+      <div className="absolute top-0 w-full h-36 md:h-40 bg-gradient-to-br from-[#FF5593] to-[rgb(255,117,119)] rounded-b-[60px] z-10">
         <Header title="이벤트 등록" onBack={onPrev} textColor="text-white" />
       </div>
 
       {/* 레이아웃 내용 */}
-      <div className="relative flex flex-col justify-between -translate-y-16 w-[85%] min-h-[81vh] bg-white rounded-[20px]">
+      <div className="flex flex-col justify-between w-[85%] min-h-[calc(100vh-6rem)] bg-white rounded-[20px] mt-24 mx-auto z-20">
         <div>
           <div className="text-center w-full my-8 text-lg md:text-2xl font-bold">{title}</div>
-          <div className={`relative ${className}`}>{children}</div>
+          <div className={`${className}`}>{children}</div>
         </div>
-        <div className="sticky bottom-0 left-0 w-full p-5">
+        <div className="w-full p-5">
           <Button label="다음" onClick={onNext} className="w-full h-12 rounded-full" />
         </div>
       </div>
