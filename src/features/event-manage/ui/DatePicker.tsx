ef7ca8ed99d35562fa815e7e3bc7949a25fe6ad4
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-19 17:19:18 +0900

    feat: window 크기 변경을 감지하는 useWindowSize 커스텀 훅 구현

--- src/features/event-manage/ui/DatePicker.tsx
+++ src/features/event-manage/ui/DatePicker.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import { ko } from 'date-fns/locale';
 import 'react-datepicker/dist/react-datepicker.css';
+import useWindowSize from '../../../shared/lib/useWindowSizeHook';
 
 interface DatePickerProps {
   title: string;
@@ -10,6 +11,9 @@ interface DatePickerProps {
 }
 
 const EventDatePicker: React.FC<DatePickerProps> = ({ title, textSize, className }) => {
+  const [windowSize] = useWindowSize();
+  const [width, setWidth] = useState(false);
+
   const [startDate, setStartDate] = useState<Date | undefined>(new Date());
   const [endDate, setEndDate] = useState<Date | undefined>(new Date());
   const [startTime, setStartTime] = useState<string>('06:00');
@@ -29,88 +33,98 @@ const EventDatePicker: React.FC<DatePickerProps> = ({ title, textSize, className
 
   const timeOptions = generateTimeOptions();
 
+  useEffect(() => {
+    setWidth(windowSize.width <= 400);
+  }, [windowSize]);
+
   return (
     <div className="flex flex-col w-full">
       <h3 className={`text-black mb-5 font-semibold ${textSize} ${className}`}>{title}</h3>
-      <div className="flex justify-center items-center gap-1">
-        <div className="flex gap-1">
-          <DatePicker
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-            locale={ko}
-            dateFormat="MM월 dd일"
-            className="w-24 h-10 border border-placeholderText rounded-[5px] p-2"
-            renderCustomHeader={({
-              date,
-              decreaseMonth,
-              increaseMonth,
-              prevMonthButtonDisabled,
-              nextMonthButtonDisabled,
-            }) => (
-              <div className="flex justify-center gap-4">
-                <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} className="mb-1">
-                  &lt;
-                </button>
-                <span>
-                  {date.getFullYear()}년 {date.getMonth() + 1}월
-                </span>
-                <button onClick={increaseMonth} disabled={nextMonthButtonDisabled} className="mb-1">
-                  &gt;
-                </button>
-              </div>
-            )}
-          />
-          <select
-            value={startTime}
-            onChange={e => setStartTime(e.target.value)}
-            className="w-24 h-10 border border-placeholderText rounded-[5px] p-2"
-          >
-            {timeOptions.map(time => (
-              <option key={time} value={time}>
-                {time}
-              </option>
-            ))}
-          </select>
+      <div className={`${width ? 'flex flex-col space-y-4' : 'flex justify-center items-center gap-1'}`}>
+        <div>
+          {width && <span className="text-xs font-bold">시작 날짜</span>}
+          <div className="flex gap-1">
+            <DatePicker
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              locale={ko}
+              dateFormat="MM월 dd일"
+              className="w-20 h-9 md:w-24 md:h-10 border border-placeholderText text-sm md:text-md rounded-[5px] p-2"
+              renderCustomHeader={({
+                date,
+                decreaseMonth,
+                increaseMonth,
+                prevMonthButtonDisabled,
+                nextMonthButtonDisabled,
+              }) => (
+                <div className="flex justify-center gap-4">
+                  <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} className="mb-1">
+                    &lt;
+                  </button>
+                  <span>
+                    {date.getFullYear()}년 {date.getMonth() + 1}월
+                  </span>
+                  <button onClick={increaseMonth} disabled={nextMonthButtonDisabled} className="mb-1">
+                    &gt;
+                  </button>
+                </div>
+              )}
+            />
+            <select
+              value={startTime}
+              onChange={e => setStartTime(e.target.value)}
+              className="w-20 h-9 md:w-24 md:h-10 border border-placeholderText text-sm md:text-md rounded-[5px] p-2"
+            >
+              {timeOptions.map(time => (
+                <option key={time} value={time}>
+                  {time}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="text-lg">&gt;</div>
-        <div className="flex gap-1">
-          <DatePicker
-            selected={endDate}
-            onChange={date => setEndDate(date)}
-            locale={ko}
-            dateFormat="MM월 dd일"
-            className="w-24 h-10 border border-placeholderText rounded-[5px] p-2"
-            renderCustomHeader={({
-              date,
-              decreaseMonth,
-              increaseMonth,
-              prevMonthButtonDisabled,
-              nextMonthButtonDisabled,
-            }) => (
-              <div className="flex justify-center gap-4">
-                <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} className="mb-1">
-                  &lt;
-                </button>
-                <span>
-                  {date.getFullYear()}년 {date.getMonth() + 1}월
-                </span>
-                <button onClick={increaseMonth} disabled={nextMonthButtonDisabled} className="mb-1">
-                  &gt;
-                </button>
-              </div>
-            )}
-          />
-          <select
-            value={endTime}
-            onChange={e => setEndTime(e.target.value)}
-            className="w-24 h-10 border border-placeholderText rounded-[5px] p-2"
-          >
-            {timeOptions.map(time => (
-              <option key={time} value={time}>
-                {time}
-              </option>
-            ))}
-          </select>
+        {!width && <div className="text-lg">&gt;</div>}
+        <div>
+          {width && <span className="text-xs font-bold">종료 날짜</span>}
+          <div className="flex gap-1">
+            <DatePicker
+              selected={endDate}
+              onChange={date => setEndDate(date)}
+              locale={ko}
+              dateFormat="MM월 dd일"
+              className="w-20 h-9 md:w-24 md:h-10 border border-placeholderText text-sm md:text-md rounded-[5px] p-2"
+              renderCustomHeader={({
+                date,
+                decreaseMonth,
+                increaseMonth,
+                prevMonthButtonDisabled,
+                nextMonthButtonDisabled,
+              }) => (
+                <div className="flex justify-center gap-4">
+                  <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} className="mb-1">
+                    &lt;
+                  </button>
+                  <span>
+                    {date.getFullYear()}년 {date.getMonth() + 1}월
+                  </span>
+                  <button onClick={increaseMonth} disabled={nextMonthButtonDisabled} className="mb-1">
+                    &gt;
+                  </button>
+                </div>
+              )}
+            />
+            <select
+              value={endTime}
+              onChange={e => setEndTime(e.target.value)}
+              className="w-20 h-9 md:w-24 md:h-10 border border-placeholderText text-sm md:text-md rounded-[5px] p-2"
+            >
+              {timeOptions.map(time => (
+                <option key={time} value={time}>
+                  {time}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
     </div>
